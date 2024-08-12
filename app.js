00191b71fde4069818ada818367c92e9e1718c48
unknown <dhavalvaghasiya2012@gmail.com>
2024-08-12 10:14:54 +0530

    2 commit

--- app.js
+++ app.js
@@ -1,40 +1,65 @@
-const express = require("express");
-const morgan = require("morgan");
-const app = express();
-const users = require("./dummy.json");
-// console.log(users);
+// ------------------------------------------- lec_1
 
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// const express = require('express');
+// const app = express();
 
-app.get("/", (req , res) => {
-    res.send("welcome to epress server");
-});
+// app.get('/', (req, res) => {
+//     res.send('Welcome to Express');
+//     res.status(200);
+//     res.end();
+// });
+// app.get('/login', (req, res) => {
+//     res.json({ msg: 'Welcome to Login Page' });
+//     res.end();
+// });
+// app.post('/login', (req, res) => {
+//     res.send('Welcome to Login Post method');
+//     res.end();
+// });
+// app.listen(2304, () => {
+//     console.log('Server is connected at http://localhost:2304');
+// });
 
-// CRUD
-// Creat User
-app.post("/user", (req , res) => {
-    //  console.log(req.body);
-    users.push(req.body);
-    res.json({message: "user added success" });
-});
 
-// READ - get all users
-app.get("/user", (req, res) => {
-    res.json(users);
-});
+// ------------------lec_4------------------
 
-// get single user
-app.get("/user/:id", (req, res) => {
-    let id = +req.params.id;
-    let item = users.find((user)=>user.id === id)
-    res.json(item);
-});
+// const express = require("express");
+// const morgan = require("morgan");
+// const app = express();
+// const users = require("./dummy.json");
+// // console.log(users);
 
-app.listen(2006, () => {
-    console.log(`server start at http://localhost:2006`);   
-});
+// app.use(morgan("dev"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+// app.get("/", (req , res) => {
+//     res.send("welcome to epress server");
+// });
+
+// // CRUD
+// // Creat User
+// app.post("/user", (req , res) => {
+//     //  console.log(req.body);
+//     users.push(req.body);
+//     res.json({message: "user added success" });
+// });
+
+// // READ - get all users
+// app.get("/user", (req, res) => {
+//     res.json(users);
+// });
+
+// // get single user
+// app.get("/user/:id", (req, res) => {
+//     let id = +req.params.id;
+//     let item = users.find((user)=>user.id === id)
+//     res.json(item);
+// });
+
+// app.listen(2006, () => {
+//     console.log(`server start at http://localhost:2006`);   
+// });
 
 
 
--- server.js
+++ server.js
@@ -1,24 +1,49 @@
-// ------------------------------------------- lec_1
+// ----------------------------------------------- lec_2
 
 // const express = require('express');
 // const app = express();
+// // const data = require('./friend.json');
+// const fs = require('fs');
+// const data = fs.readFilesync('./friend.json', 'utf-8');
+// // console.log(data);
 
-// app.get('/', (req, res) => {
-//     res.send('Welcome to Express');
-//     res.status(200);
+// // POST, GET, PUT, PATCH, DELETE
+// Server.get("/", (req,res)=>{
+//     res.write('Welcome to Express Server');
 //     res.end();
-// });
-// app.get('/login', (req, res) => {
-//     res.json({ msg: 'Welcome to Login Page' });
+// })
+
+// // Server.get("/", (req,res)=>{
+// //     res.write('GET Method - 1');
+// //     res.end();
+// // })
+
+// Server.post("/", (req,res)=>{
+//     // res.write('Welcome to Post Method');
+//     res.send('<h1>Send Method</h1>');
+// })
+
+// server.put("/", (req,res)=>{
+//     res.json({message: 'Hello'});
+// })
+
+// server.patch("/", (res,req)=>{
+//     res.status(400);
+//     res.json({message:'Hello'})
+// })
+
+// server.get("/user", (req,res)=>{
+//     res.json(json.parse(data));
+// })
+
+// serever.get("/login", (req,res)=>{
+//     res.write('Welcome to Login Page');
 //     res.end();
-// });
-// app.post('/login', (req, res) => {
-//     res.send('Welcome to Login Post method');
-//     res.end();
-// });
-// app.listen(2304, () => {
-//     console.log('Server is connected at http://localhost:2304');
-// });
+// })
+
+// server.listen(2304, ()=>{
+//     console.log('Server Start at http://localhost:2304');   
+// })
 
 
 
