8fe4254a70087dc2a00b649add3df24168dea390
unknown <dhavalvaghasiya2012@gmail.com>
2024-08-17 17:02:28 +0530

    8-lec

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -132,48 +132,89 @@
 //     res.json({ message: "Users Delete Success" });
 // };
 
-const User = require("../model/user.model");
+// const User = require("../model/user.model");
 
-exports.addNewUser = async (req, res) => {
-   try {
-    let user = await User.findOne({email: req.body.email});
-    //console.log(user);
-    if(user){
-        return res.status(400).json({message: 'User already exist.....'});
-    }
-    user = await User.create(req.body);
-    res.status(201).json({ user, message: "User Added Success"});
-   }catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Internal Server Error"});
-   }
-};
+// exports.addNewUser = async (req, res) => {
+//    try {
+//     let user = await User.findOne({email: req.body.email});
+//     //console.log(user);
+//     if(user){
+//         return res.status(400).json({message: 'User already exist.....'});
+//     }
+//     user = await User.create(req.body);
+//     res.status(201).json({ user, message: "User Added Success"});
+//    }catch (err) {
+//     console.log(err);
+//     res.status(500).json({ message: "Internal Server Error"});
+//    }
+// };
                          
-exports.getAllUsers = async (req, res) => {
-    try{
-        let users = await User.find();
-        res.ststus(200).json(users);
-    }catch (err) {
-        console.log(err);
-        res.status(500).json({ message: "Internal server Error"});
-    }
-};
+// exports.getAllUsers = async (req, res) => {
+//     try{
+//         let users = await User.find();
+//         res.ststus(200).json(users);
+//     }catch (err) {
+//         console.log(err);
+//         res.status(500).json({ message: "Internal server Error"});
+//     }
+// };
 
                
-exports.getUser = async (req, res) => {
-    try{
-        // let user = await User.findOne({firstName: req.body.firstName});
-        // let user = await User.findOne({_id: req.query.userId});
-       let user = await User.findById(req.query.userId);
-       console.log(user);
-       if(!user) {
-        return res.status(404).json({ message: "User not found"});
-       }
-       res.status(200).json(user);
-    }catch (err) {
+// exports.getUser = async (req, res) => {
+//     try{
+//         // let user = await User.findOne({firstName: req.body.firstName});
+//         // let user = await User.findOne({_id: req.query.userId});
+//        let user = await User.findById(req.query.userId);
+//        console.log(user);
+//        if(!user) {
+//         return res.status(404).json({ message: "User not found"});
+//        }
+//        res.status(200).json(user);
+//     }catch (err) {
+//         console.log(err);
+//         res.status(500).json({ message: "Internal Server Error" });
+//     }
+// };
+
+
+
+exports.updateUser = async (req, res) => {
+    try {
+        let user = await User.findById(req.query.userId);
+        // console.log(user);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        //  user = await User.updateOne({ _id: user._id }, req.body, { new: true });
+        // user = await user.findOneAndUpdate({ _id: user._id }, req.body, { new: true });
+        user = await user.findByIdAndUpdate(user._id, { $set: req.body }, { new: true });
+        res.status(202).json({ user, message: 'user update success' });
+    } catch (err) {
         console.log(err);
         res.status(500).json({ message: "Internal Server Error" });
     }
 };
+
+exports.deleteUser = async (req, res) => {
+    try {
+        let user = await user.findById(req.query.userId);
+        // console.log(user);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        // user = await User.deleteOne({ _id: user._id })
+        user = await User.findOneAndDelete({ _id: user._id })
+        // user = await USer.findByIdAndDelete(user_id)
+        res.status(200).json({ user, message: 'User Delete Success' });
+    } catch (err) {
+        console.log(err);
+        res.ststus(500).json({ message: "Interval Server Error" });
+    }
+}
+
+
+
+
+
 
 
