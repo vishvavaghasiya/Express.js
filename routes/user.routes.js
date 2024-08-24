5c64c4b242d0628d23756fd4de0b735390edee74
unknown <dhavalvaghasiya2012@gmail.com>
2024-08-24 09:39:12 +0530

    12-lec

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,22 @@
-const express = require("express");
+const express = require('express');
 const userRoutes = express.Router();
-const {
-    registerUser, loginUser, getprofile
-} = require("../controller/user.controller");
-const {verifyToken} = require('../helpers/verifyToken');
 
-userRoutes.post('/register', registerUser);
-userRoutes.post('/login', loginUser);
-userRoutes.get('/me', verifyToken, getprofile);
+const {
+   registerUser , loginUser , getProfile ,updateProfile , deleteUser , updatePassword
+} = require('../controller/user.controller');
+const {verifyToken} = require('../helpers/verifyToken')
+
+userRoutes.post('/registerUser' , registerUser);
+
+userRoutes.post('/loginUser' , loginUser);
+
+userRoutes.get('/profile', verifyToken,getProfile);
+
+userRoutes.put('/update', verifyToken,updateProfile);
+
+userRoutes.delete('/deleteUser' ,verifyToken , deleteUser);
+
+userRoutes.put('/Update-passwordUser',verifyToken,updatePassword);
+
 
 module.exports = userRoutes;
