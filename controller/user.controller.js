2fa70758ee79eb8ad88619666f5919f65be58411
unknown <dhavalvaghasiya2012@gmail.com>
2024-08-26 10:56:00 +0530

    13-lec

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,118 +1,149 @@
-const User = require("../model/user.model");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
+// const users = require('../user.json')  //=>static 
+const User = require('../model/user.model')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+//register user with profile image uploading
 exports.registerUser = async (req, res) => {
-  try {
-    let user = await User.findOne({ email: req.body.email, isDelete: false });
-    if (user) {
-      return res.json({ message: "User Already Exists..." });
-    }
-    let hashpassword = await bcrypt.hash(req.body.password, 10);
-    // console.log(hashpassword)
-    user = await User.create({ ...req.body, password: hashpassword });
-    res.status(201).json({ user, message: "Register Successfully..." });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Server Error" });
-  }
-};
+   try {
+      let imagePath = " "  // file not exetute path so print empty string
+      let user = await User.findOne({ email: req.body.email, isDelete: false });
+      if (user) {
+         return res.json({ message: 'User already exist...' })
+      }
+      if(req.file){
+         imagePath = req.file.path.replace(/\\/g ,"/")  // regx change path to beacuse mac and linex path does not to word this -> \\ and change using regx exprestion path change " / "
+      }
+      let hashPassword = await bcrypt.hash(req.body.password, 10)
+      user = await User.create({ ...req.body, password: hashPassword  , profileImage:imagePath})
+      res.status(201).json({ user, message: 'Register success...' })
+   }
+   catch (err) {
+      console.log(err);
+      res.status(500).json({ message: 'Server Error' })
+   }
+}
 
+//login user
 exports.loginUser = async (req, res) => {
-  try {
-    let user = await User.findOne({ email: req.body.email, isDelete: false });
-    if (!user) {
-      return res.json({ message: "User Not Found..." });
-    }
-    let comparedPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-    // console.log(comparedPassword)
-    if (!comparedPassword) {
-      return res.json({ message: "Email Or Password Not Match..." });
-    }
+   try {
+      let user = await User.findOne({ email: req.body.email, isDelete: false })
+      if (!user) {
+         return res.json({ message: 'User Not Found...' })
+      }
+      let comparedPassword = await bcrypt.compare(req.body.password, user.password)
+      if (!comparedPassword) {
+         return res.json({ message: 'Email or password does not matched...' })
+      }
+      let token = await jwt.sign({ userId: user._id }, process.env.JWT_SECRET)
+      console.log(token);// token genret to the console 
 
-    let token = await jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
-    res.status(200).json({ message: "Login Success...", token });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Server Error..." });
-  }
-};
+      res.status(200).json({ message: "Login Success...", token })
+   }
+   catch (err) {
+      console.log(err);
+      res.status(500).json({ message: 'Server Error' })
+   }
+}
 
+//get profile
 exports.getProfile = async (req, res) => {
-  try {
-    res.json(req.user);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Server Error..." });
-  }
-};
+   try {
+      // res.status(200).json({message: "Show user profile"})
+      res.json(req.user)
+   }
+   catch (err) {
+      console.log(err);
+      res.status(500).json({ message: 'Server Error' })
+   }
+}
 
+// update profile
 exports.updateProfile = async (req, res) => {
-  try {
-    let user = req.user;
-    user = await User.findByIdAndUpdate(
-      user._id,
-      { $set: req.body },
-      { new: true }
-    );
-    console.log(user);
-    res
-      .status(202)
-      .json({ user, message: "User Profile Updated............." });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Server Error..." });
-  }
-};
+   try {
+      let user = req.user;
+      user = await User.findByIdAndUpdate(user._id, { $set: req.body }, { new: true })
+      res.status(202).json({ user, message: "User profile update...." })
+   }
+   catch (err) {
+      console.log(err);
+      res.status(500).json({ message: 'Server Error' })
+   }
+}
 
+// task => soft delete
 exports.deleteUser = async (req, res) => {
-  try {
-    let user = req.user;
-    user = await User.findById(user._id, { isDelete: false });
-    if (!user) {
-      return res.json({ message: "User is not Found !!!" });
-    }
-    user = await User.findByIdAndUpdate(user._id, { isDelete: true });
-    res.json({ user, message: "User delete sucess !!" });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Server Error..." });
-  }
-};
+   try {
+      let user = req.user
+      if (!user) {
+         return res.status(404).json({ message: "user Not Found.." })
+      }
+      user = await User.findByIdAndUpdate(user._id, { isDelete: true }, { new: true })
+      res.status(200).json({ message: 'User delete successfully...' })
+   }
+   catch (err) {
+      console.log(err);
+      res.status(500).json({ message: 'Server Error' })
+   }
+}
 
-exports.updatePassword = async (req, res) => {
-  try {
-    let userId = req.user._id;
-    let { oldPassword, newPassword, confirmPassword } = req.body;
 
-    let user = req.user;
-    user = await User.findById(userId);
-    if (!user) return res.json({ message: "User is not found.." });
+// task => change password 
+exports.changePassword = async (req, res) => {
+   try {
+      let user = req.user; 
+      user = await User.findById(req.user._id); 
+      if (!user) {
+         return res.status(404).json({ message: "user Not Found.." })
+      }
+      
+      let { oldPassword, newPassword, confirmPassword } = req.body; 
+      const comparedOldPassword= await bcrypt.compare(oldPassword, user.password); 
+      if (!comparedOldPassword) 
+         return res.status(400).json({ message: "Please enter valid password" }); 
+   
+      if (oldPassword === newPassword) { 
+        return res.status(400).json({  message: "Old password and new password both are the same , Please enter valid password"}); 
+      } 
+   
+      if (newPassword !== confirmPassword) { 
+        return res.status(400).json({message:"New password and confirm password do not match"}); 
+      } 
+   
+      const hashedNewPassword = await bcrypt.hash(newPassword, 10); 
+      user = await User.findByIdAndUpdate(req.user._id, { password: hashedNewPassword }); 
+   
+      res.status(200).json({ message: "Password updated successfully!", user}); 
+   }
+   catch (err) {
+      console.log(err);
+      res.status(500).json({ message: 'Server Error' })
+   }
+}
+// corrent password =>corrent password
+// corrent password update operation -> new password
+// new password and comfirm password
 
-    const isSamePassword = await bcrypt.compare(oldPassword, user.password);
-    if (!isSamePassword) return res.json({ message: "Invalid old password" });
-
-    if (oldPassword === newPassword) {
-      return res.json({
-        message: "Old password and new password are the same",
-      });
-    }
-
-    if (newPassword !== confirmPassword) {
-      return res.json({
-        message: "New password and confirm password do not match",
-      });
-    }
-
-    const hashedNewPassword = await bcrypt.hash(newPassword, 10);
-    await User.findByIdAndUpdate(userId, { password: hashedNewPassword });
-
-    res.json({ message: "Password updated successfully!" });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Server Error..." });
-  }
-};
+exports.userShow = async(req, res) => {
+   try{
+      // let user ={
+      //    firstName : 'sachin',
+      //    lastName : 'Tendulkar',
+      //    age: 40,
+      //    email : 'schin@text.in',
+      //    mobileNo : '1234567890',
+      //    hobbies: ['sport','music','Dance','cricket']
+      // }
+      let user = await User.findOne({firstName:req.query.name, isDelete:false})
+      if(!user){
+         return res.render('notfound.ejs')
+      }
+      res.render('student.hbs',{user})
+      // res.render('user.ejs',{user})
+   }
+   catch(err){
+      console.log(err);
+      res.status(500).json({message: 'server Error'})
+   }
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,22 +1,15 @@
-const express = require('express');
-const userRoutes = express.Router();
+const express = require("express")
+const userRoutes = express.Router()
+const { registerUser, loginUser, getProfile, updateProfile, deleteUser, changePassword ,userShow } = require("../controller/user.controller")
+const { verifyToken } = require('../helpers/verifyToken')
+const { upload } = require('../helpers/imageUpload')
 
-const {
-   registerUser , loginUser , getProfile ,updateProfile , deleteUser , updatePassword
-} = require('../controller/user.controller');
-const {verifyToken} = require('../helpers/verifyToken')
-
-userRoutes.post('/registerUser' , registerUser);
-
-userRoutes.post('/loginUser' , loginUser);
-
-userRoutes.get('/profile', verifyToken,getProfile);
-
-userRoutes.put('/update', verifyToken,updateProfile);
-
-userRoutes.delete('/deleteUser' ,verifyToken , deleteUser);
-
-userRoutes.put('/Update-passwordUser',verifyToken,updatePassword);
-
+userRoutes.post('/register', upload.single('profileImage'), registerUser)
+userRoutes.post('/login', loginUser)
+userRoutes.get('/me', verifyToken, getProfile)
+userRoutes.put('/update-profile', verifyToken, updateProfile)
+userRoutes.delete('/delete-user', verifyToken, deleteUser)
+userRoutes.put('/change-password', verifyToken, changePassword)
+userRoutes.get('/', userShow)
 
 module.exports = userRoutes;
